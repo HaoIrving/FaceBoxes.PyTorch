03ef00c5dbbcddafefb2f9e34ec6d4b9ac4f71ee
sun <sun@sun>
2020-12-13 00:10:06 +0800

    db

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,3 @@
-from .sar_voc import VOCDetection, AnnotationTransform, detection_collate, load_sar_ship_instances
+from .sar_voc import VOCDetection, AnnotationTransform, detection_collate#, load_sar_ship_instances
 from .data_augment import *
 from .config import *
